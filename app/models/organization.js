9996bc7568267ec955059ac5edf52c61830768d9
Ryan <ryetoaster@gmail.com>
2018-06-16 13:08:36 -0700

    more models

---
+++ app/models/organization.js
@@ -0,0 +1,24 @@
+module.exports = function (sequelize, DataTypes) {
+    var Organization = sequelize.define("Organization", {
+        name: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        password: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+    Organization.associate = function (models) {
+        Organization.hasMany(models.Activity, {
+            onDelete: "cascade"
+        });
+    };
+    return Organization;
+}
