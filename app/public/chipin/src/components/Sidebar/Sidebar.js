9b6cc7a7d3b0a4f71c3c11610ada22debf21d97d
Roberto aguero <rjaguero00@gmail.com>
2018-06-21 21:22:12 -0700

    dashboard

---
+++ app/public/chipin/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => (
+    <nav id="sidebar">
+        {/* <div className="sidebar-header">
+            <h3>Organization/User Name</h3>
+        </div> */}
+
+        <ul className="list-unstyled components">
+            <p>Organization/User Name</p>
+            <li className="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">About</a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
+                <ul className="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Sidebar;
