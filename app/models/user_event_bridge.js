6a850d338107331d33d49bba8aa29980769ae107
EvelynCude <ebenitez@email.arizona.edu>
2018-06-26 20:47:33 -0700

    back end model glitches resoled

--- app/models/user_event_bridge.js
+++ app/models/user_event_bridge.js
@@ -4,26 +4,14 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.INTEGER,
         },
         points: {
-<<<<<<< HEAD
-            type: DataType.INTEGER,
-        },
-        IsValid: {
-            defaultValue: false
-=======
             type: DataTypes.INTEGER,
->>>>>>> 5ece2f201341b9208d761cbd707163453d5d3e2f
         }
 
     });
 
     User_Event_Bridge.associate = function (models) {
-<<<<<<< HEAD
-        User_Event_Bridge.hasOne(models.Activity);
-        User_Event_Bridge.hasMany(models.User);
-=======
         User_Event_Bridge.hasMany(models.activity),
             User_Event_Bridge.hasMany(models.User)
->>>>>>> 5ece2f201341b9208d761cbd707163453d5d3e2f
     };
     return User_Event_Bridge;
 }
